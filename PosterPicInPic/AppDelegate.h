ed352de8819163177e9f4ecd265ece094dbd9a44
精神薇 <zhengwei931102@163.com>
2020-04-03 09:23:48 +0800

    Initial Commit

---
+++ PosterPicInPic/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PosterPicInPic
+//
+//  Created by zw on 2020/4/3.
+//  Copyright © 2020 zw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PosterPicInPic/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  PosterPicInPic
+//
+//  Created by zw on 2020/4/3.
+//  Copyright © 2020 zw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
